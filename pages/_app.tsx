34c75e78a167d0c059704e68c18a2ac989d9c146
Ivan <wuchitho1@hotmail.com>
2023-02-13 17:57:40 +0800

    auth context done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import {Toaster} from 'react-hot-toast';
+import { UserContext } from "../lib/context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />
+    <UserContext.Provider value={{user:{},username:'jeff'}}>
+    <Navbar />
       <Component {...pageProps} />
       <Toaster/>
+    </UserContext.Provider>
     </>
   );
 }
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,7 +1,41 @@
+import { auth, googleAuthProvider } from "../lib/firebase";
+import { useContext } from "react";
+import { UserContext } from "../lib/context";
+
 export default function EnterPage({}) {
+const {user,username}=useContext(UserContext)
+
   return (
     <main>
-      <h1>Sign up</h1>
+      {user ? (
+        !username ? (
+          <UsernameForm />
+        ) : (
+          <SignOutButton />
+        )
+      ) : (
+        <SignInButton />
+      )}
     </main>
   );
+}
+
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await auth.signInWithPopup(googleAuthProvider);
+  };
+
+  return (
+    <button className="btn-google" onClick={signInWithGoogle}>
+      <img src={"/google.png"} />Sign in with googlge
+    </button>
+  );
+}
+
+function SignOutButton() {
+  return <button onClick={() => auth.signOut()}>Sign out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
