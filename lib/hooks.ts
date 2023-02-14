220219b41f00a3371df022b3c1a49eb527697fdc
Ivan <wuchitho1@hotmail.com>
2023-02-14 11:15:41 +0800

    custom username done

---
+++ lib/hooks.ts
@@ -0,0 +1,26 @@
+import { auth, firestore } from "../lib/firebase";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export function useUserData() {
+  const [user] = useAuthState(auth);
+  const [username, setUserName] = useState(null);
+
+  useEffect(() => {
+    let unsubscribe;
+
+    if (user) {
+      const ref = firestore.collection("users").doc(user.uid);
+      unsubscribe = ref.onSnapshot((doc) => {
+        setUserName(doc.data()?.username);
+        console.log(doc.data()?.username);
+      });
+    } else {
+      setUserName(null);
+    }
+
+    return unsubscribe;
+  }, [user]);
+
+  return { user, username };
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 import { UserContext } from "../lib/context";
 
+import { useUserData } from "../lib/hooks";
+
 export default function App({ Component, pageProps }: AppProps) {
+
+  const userData = useUserData();
+
   return (
     <>
-    <UserContext.Provider value={{user:{},username:'jeff'}}>
-    <Navbar />
-      <Component {...pageProps} />
-      <Toaster/>
-    </UserContext.Provider>
+      <UserContext.Provider value={userData}>
+        <Navbar />
+        <Component {...pageProps} />
+        <Toaster />
+      </UserContext.Provider>
     </>
   );
 }
